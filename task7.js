846f78594e0d31db3152a5d9f91e6a74f5007846
Arman Eroyan <armaneroyan84@gmail.com>
2026-02-27 14:19:22 +0400

    DO SOMETING

--- task7.js
+++ task7.js
@@ -1,9 +1,5 @@
 function sum_Elememts(arr){
-
-    if(arr.length < 1){
-        return 0;
-    }
-
+    
     let sum = 0;
 
     for(let i = 0; i < arr.length; ++i){
@@ -13,6 +9,6 @@ function sum_Elememts(arr){
     return sum;
 }
 
-let numList = [3, 6, 9, 12];
+let numList = [];
 console.log(sum_Elememts(numList)); 
 
