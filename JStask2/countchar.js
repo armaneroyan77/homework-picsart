ffe5efa6e7a0660892bfb5fac92a2599958662fa
Arman Eroyan <armaneroyan84@gmail.com>
2026-03-18 21:49:28 +0400

    changed countchar.js

--- JStask2/countchar.js
+++ JStask2/countchar.js
@@ -1,7 +1,7 @@
 function countChar(str, char, index){
 
     if(index < 0){
-        return 1;
+        return 0;
     }
 
     if(str[index] !== char){
@@ -11,5 +11,5 @@ function countChar(str, char, index){
     return 1 + countChar(str, char, index - 1);
 }
 
-let str = "lhelllosdl";
-console.log(countChar(str, "l", str.length - 1));
+let str = "lfhelllosffdlf";
+console.log(countChar(str, "f", str.length - 1));
