9b3bc76cc0708e1c427447430c2987ae8d737d3e
Arman Eroyan <armaneroyan84@gmail.com>
2026-04-19 13:33:05 +0400

    changed

--- JStask11/iterTask1.js
+++ JStask11/iterTask1.js
@@ -1,23 +1,21 @@
-const myRange = {
+const myRange = { 
+
     from: 1,
     to: 20,
 
     [Symbol.iterator](){
+
         let current = this.to;
 
         return {
 
             next: () => {
-
-                if(current >= this.from){
-                    return {value: current--, done:false};
-                }
-                else{
-                    return {done: true};
-                }
+                if(current >= this.from) return {value: current--, done:false};
+                else return {done: true};
             }
 
         }
+
     }
 
 }
@@ -29,10 +27,8 @@ console.log(iter.next());
 console.log(iter.next());
 console.log(iter.next());
 
-
 const arr = [...myRange];
 console.log(arr);
-
 
 for(let val of myRange){
     console.log(val);
